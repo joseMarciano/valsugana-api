f929eee54d51cda86c8787fdc2f01f4d032738a6
joseMarciano <marcianojosepaulo@gmail.com>
2021-04-27 22:52:15 -0300

    Inicio do projeto

---
+++ config/server/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+
+module.exports = express()
+    .use(express.json());
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const server = require('../config/server');
+
+
+
+
+
+server.listen(3000,() => console.log('Server is running'))
