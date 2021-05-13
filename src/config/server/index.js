df803e2307c2c6f30df404ac4fcab8b9f412c64b
joseMarciano <marcianojosepaulo@gmail.com>
2021-05-13 08:35:48 -0300

    Criando teste de conexão

--- src/config/server/index.js
+++ src/config/server/index.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const {sequelize} = require('../../api/models/index');
+
+(async function() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+})();
 
 
 module.exports = express()
