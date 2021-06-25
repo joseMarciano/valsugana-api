f3eb8c4d2b07ca9811e78950ac7c71b3eea813a8
joseMarciano <marcianojosepaulo@gmail.com>
2021-06-24 22:09:44 -0300

    CORS()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const server = require('./config/server');
+const cors = require('cors');
 const mainRoute = require('./api/routes');
 
 (async function () {
     server
-        .use('/api', mainRoute)
+        .use('/api',cors(), mainRoute)
         .listen(process.env.PORT || 3000, () => console.log('Server is running'))
-
-
 })()
